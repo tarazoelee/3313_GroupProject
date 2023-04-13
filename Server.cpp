b5dfa2a80c7cbae96f1f01c43ca5187ab8ee11d8
Tara Lee <taralee@Taras-MacBook-Pro.local>
2023-04-13 19:43:44 -0400

    idk trying

--- Server.cpp
+++ Server.cpp
@@ -20,6 +20,124 @@
 
 using namespace Sync;
 
+// This thread handles the connection to the server
+class ClientThread : public Thread
+{
+private:
+  // Reference to our connected socket
+  Socket &socket;
+
+  // Reference to boolean flag for terminating the thread
+  bool &terminate;
+
+  // Are we connected?
+  bool connected = false;
+
+  // Data to send to server
+  ByteArray data;
+  std::string data_str;
+  int expectedLength = 0;
+
+public:
+  ClientThread(Socket &socket, bool &terminate)
+      : socket(socket), terminate(terminate)
+  {
+  }
+
+  ~ClientThread()
+  {
+  }
+
+  void TryConnect()
+  {
+    try
+    {
+      std::cout << "Connecting...";
+      std::cout.flush();
+      int result = socket.Open();
+      connected = true;
+      std::cout << "OK" << std::endl;
+    }
+    catch (std::string exception)
+    {
+      std::cout << "FAIL (" << exception << ")" << std::endl;
+      return;
+    }
+  }
+
+  virtual long ThreadMain()
+  {
+    // Initially we need a connection
+    while (true)
+    {
+      // Attempt to connect
+      TryConnect();
+
+      // Check if we are exiting or connection was established
+      if (terminate || connected)
+      {
+        break;
+      }
+
+      // Try again every 5 seconds
+      std::cout << "Trying again in 5 seconds" << std::endl;
+      sleep(5);
+    }
+
+    while (!terminate)
+    {
+      // We are connected, perform our operations
+      std::cout << "Please input your data (done to exit): ";
+      std::cout.flush();
+
+      // Get the data
+      data_str.clear();
+      std::getline(std::cin, data_str);
+      data = ByteArray(data_str);
+      expectedLength = data_str.size();
+
+      // Must have data to send
+      if (expectedLength == 0)
+      {
+        std::cout << "Cannot send no data!" << std::endl;
+        continue;
+      }
+      else if (data_str == "done")
+      {
+        std::cout << "Closing the client..." << std::endl;
+        terminate = true;
+        break;
+      }
+
+      // Write to the server
+      socket.Write(data);
+
+      // Get the response
+      socket.Read(data);
+      data_str = data.ToString();
+
+      // If the length is different, the socket must be closed
+      if (data_str.size() != expectedLength)
+      {
+        std::cout << "Server failed to respond. Closing client..." << std::endl;
+        terminate = true;
+      }
+      else
+      {
+        std::cout << "Server Response: " << data_str << std::endl;
+      }
+
+      // Are we terminating
+      if (terminate)
+      {
+        break;
+      }
+    }
+
+    return 0;
+  }
+};
+
 // extends Thread class. Responsible for handling client connections.
 class SocketThread : public Thread
 {
@@ -56,26 +174,26 @@ public:
           otherSocket->Write(response);
         }
       }
-      if (response == "CLOSE")
-      {
-        // Close the socket and remove it from the list of connected sockets
-        for (auto socket : sockets)
-        {
-          try
-          {
-            socket->Close(); // assuming Close() is a member function of the Socket class that closes the socket
-            sockets.remove(socket);
-          }
-          catch (...)
-          {
-            // ignore errors when closing sockets
-          }
-        }
+      // if (response == "CLOSE")
+      // {
+      //   // Close the socket and remove it from the list of connected sockets
+      //   for (auto socket : sockets)
+      //   {
+      //     try
+      //     {
+      //       socket->Close(); // assuming Close() is a member function of the Socket class that closes the socket
+      //       sockets.remove(socket);
+      //     }
+      //     catch (...)
+      //     {
+      //       // ignore errors when closing sockets
+      //     }
+      //   }
 
-        // Clear the list of sockets
-        sockets.clear();
-        break;
-      }
+      //   // Clear the list of sockets
+      //   sockets.clear();
+      //   break;
+      // }
     }
 
     // Close the socket and remove it from the list of connected sockets when killThread is set to true
